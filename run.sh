ce188460a2ef4b19c726c8608b1427d3078e62af
Robin Schroer <git@sulami.xyz>
2022-06-01 09:13:00 +0000

    Escape special characters
    % is used for format strings in awk's printf
    " needs to be escaped for SQLite's CSV import

--- run.sh
+++ run.sh
@@ -31,6 +31,8 @@ for repo in "${repos[@]}"; do
      git log --pretty="MARK%h|%aI|%aN|%s|" --shortstat | \
          tr '\n\  ' ' ' | \
          sed 's/MARK/\n/g' | \
+         sed 's/%/%%/g' | \
+         sed 's/"/\\"/g' | \
          grep '.' | \
          gawk -v service="$(basename $repo)" -f $SCRIPT) >> $CSV_BRIDGE
 done
